05b5e630304e2bb13c3480f5e95f42f3fc7c0702
Krishna <ks3259265@gmail.com>
2025-10-26 13:36:25 +0530

    hat diya gaya chatbot

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-GEMINI_API = "AIzaSyBHyiMX-EZwVo4G_NSOGGMu4itjKoguRmA"
-GEMINI_API_KEY = os.environ.get("GEMINI_API")
-GEMINI_API_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={GEMINI_API_KEY}"
-# Track IP message count
